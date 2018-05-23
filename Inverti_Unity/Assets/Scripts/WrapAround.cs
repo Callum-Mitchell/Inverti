9097e06543a65d2dc1032a0037649385be8daf7b
Callum <mitccall@gmail.com>
2018-05-23 05:32:34 -0400

    Added wrap-around capabilities for player

---
+++ Inverti_Unity/Assets/Scripts/WrapAround.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapAround : MonoBehaviour {
+
+    private Transform player;
+    public float leftBoundary;
+    public float rightBoundary;
+    private static float wrapDistance = 72f;
+	// Use this for initialization
+	void Start () {
+        player = transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (transform.position.x < leftBoundary) {
+            transform.Translate(new Vector3(wrapDistance, 0, 0), Space.World);
+        }
+        else if (transform.position.x > 36) {
+            transform.Translate(new Vector3(-wrapDistance, 0, 0), Space.World);
+        }
+
+    }
+
+}
