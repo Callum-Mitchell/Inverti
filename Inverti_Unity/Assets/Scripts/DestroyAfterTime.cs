3e1e0edf6b7ad4e35315bc31aae3e42a8b3620fa
Callum <mitccall@gmail.com>
2018-05-26 04:37:45 -0300

    altered spawner/wave naming convention, added DestroyAfterTime script, and modified test scene to include +ve/-ve walls

---
+++ Inverti_Unity/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+    public int lifetime = 200;
+    private int remainingLifetime;
+
+	// Use this for initialization
+	void Awake () {
+        remainingLifetime = lifetime;
+	}
+	
+	// FixedUpdate is called once per deltatime
+	void FixedUpdate () {
+        if (remainingLifetime <= 0) {
+            Destroy(gameObject);
+        }
+        remainingLifetime--;
+	}
+}
